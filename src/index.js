7dd32e660f17b590247327782d69351849df674a
Anna Chebysheva <anna.chebysheva@gmail.com>
2023-08-03 15:18:02 +0200

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+  let result = '';
+  let module = Math.abs(n);
+  let str = String(module);
+  let i = str.length;
   
+  while ( i > 0 ) {
+    result = result + str[i - 1];
+    i = i - 1;
+  }
+  return result;
 }
